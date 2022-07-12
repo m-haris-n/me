07a790d05479ee4d82158d25642a112bad14a47b
M. Haris Nabeel <m.haris.nabeel@gmail.com>
2022-07-12 03:59:57 -0700

    under construction status

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -3,10 +3,11 @@ import Container from "./Containers/Container";
 
 export default function Navbar() {
   return (
-    <Container classes="my-3 md:my-0 h-20 flex md:flex-row flex-col justify-between items-center absolute top-0">
+    <Container classes="my-3 md:my-0 h-20 flex lg:flex-row flex-col justify-between items-center absolute top-0">
       <div id="logo">
         <p className="sub-heading-med-m">m-haris-n</p>
       </div>
+      <p className="text-body-med-l my-5">/ * Under Construction * /</p>
       <div
         id="menu"
         className="flex flex-row lg:w-2/6 md:w-3/6 w-full justify-between"
