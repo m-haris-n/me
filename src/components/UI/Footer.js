6640e5383b6d82016e090890f98241686ffa9360
M. Haris Nabeel <m.haris.nabeel@gmail.com>
2022-07-21 11:18:09 -0700

    updated footer

--- src/components/UI/Footer.js
+++ src/components/UI/Footer.js
@@ -3,7 +3,7 @@ import Socialbar from "../UI/Socialbar";
 
 export default function Footer() {
   return (
-    <div className="footer w-full flex flex-col sm:flex-row justify-between items-center border-t-[2px] border-t-white py-8 mt-8">
+    <div className="footer w-full flex flex-col-reverse sm:flex-row justify-between items-center border-t-[2px] border-t-white py-8 mt-8">
       <div className="w-5/6 sm:w-2/6 logo-area h-40 flex flex-col justify-between items-center sm:items-start">
         <p className="sub-heading-l">m-haris-n</p>
         <p className="text-body-reg-l mb-5">A Full Stack Web Developer</p>
